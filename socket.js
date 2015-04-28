ec1a29a7937620553c2dc3ecbcfe6996684dc6f3
shrikanth_kr <shrikanth_kr@yahoo.com>
2015-04-28 17:23:16 +0530

    override defaults

--- socket.js
+++ socket.js
@@ -2,6 +2,13 @@ var port = process.env.PORT || 3000;
 var redis_port = process.env.REDIS_PORT;
 var redis_host = process.env.REDIS_HOST ;
  console.log('redis:'+redis_host);
+ if (redis_host) {
+  // use production (Heroku) redis configuration
+  // overwrite config to keep it simple
+  config.redis.port = redis_port;
+  config.redis.host = redis_host;
+  config.redis.password =  process.env.REDIS_AUTH;
+}
 var redis = require('redis').createClient(redis_port, redis_host, { detect_buffers: true, auth_pass: process.env.REDIS_AUTH + ""  });
 
 var adapter = require('socket.io-redis');
